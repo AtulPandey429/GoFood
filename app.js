708f578570adfaba216ac83b0c111004edbf4cb6
AtulPandey429 <pandeyatul429x@gmail.com>
2023-09-01 10:27:41 +0530

    ....

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use(express.json());
 app.use("/api/user", require("./routers/CreateUser"));
 app.use("/api/user", require("./routers/Display"));
 app.use("/api/user", require("./routers/Order"));
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
